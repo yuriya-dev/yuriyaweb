04e106954c260342769efd09c46a88cc31bd3d06
yuriya-dev <143408295+yuriya-dev@users.noreply.github.com>
2023-09-27 10:55:51 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,109 @@
+// Toggle Class Active untuk hamburger menu
+const navbarNav = document.querySelector ('.navbar-nav');
+// Ketika hamburger menu diklik
+document.querySelector('#hamburger-menu').onclick = (e) => {
+    navbarNav.classList.toggle('active');
+    e.preventDefault();
+};
+
+// Klik di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
+
+
+//  nav
+
+function scrollValue() {
+    var navbar = document.getElementById('navbar');
+    var scroll = window.scrollY;
+    if (scroll < 100) {
+        navbar.classList.remove('BgColour');
+    } else {
+        navbar.classList.add('BgColour');
+    }
+}
+
+window.addEventListener('scroll', scrollValue);
+
+
+// Slider
+var TrandingSlider = new Swiper('.mySwiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+});
+
+
+// Contact
+
+   function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      number: document.getElementById("number").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_h7zdopj";
+    const templateID = "template_mfxovac";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("number").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+
+
+// Modal Box
+const itemDetailModal = document.querySelector('#detail-modal');
+const itemDetailButtons = document.querySelectorAll('#detail-button');
+
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    }
+})
+
+// Klik tombol close modal
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+}
+
+// Klik di luar modal
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    }
+}
